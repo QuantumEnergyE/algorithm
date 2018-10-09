b0336c4f57318a23faae66c4788d8c7f56b5ffcd
QuantumEnergyE <quantumenergye@gmail.com>
2018-10-09 16:34:58 +0800

    sort

---
+++ sort.py
@@ -0,0 +1,13 @@
+def quick_sort(data):
+    pass
+
+
+def bubble_sort(data):
+    is_sorted = False
+    pos = len(data) - 1
+    while not is_sorted:
+        is_sorted = True
+        for i in range(pos):
+            if data[i] > data[i + 1]:
+                data[i], data[i + 1] = data[i + 1], data[i]
+                is_sorted = False
